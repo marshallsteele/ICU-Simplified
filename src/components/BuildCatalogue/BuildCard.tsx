91bd6d77b827ac9d498349e8ca00aa45c416f50e
Marshall Steele <marshallsteele123@hotmail.com>
2024-06-28 11:30:07 -0400

    Fixed offset card

--- src/components/BuildCatalogue/BuildCard.tsx
+++ src/components/BuildCatalogue/BuildCard.tsx
@@ -3,7 +3,7 @@ import { build } from "../Helpers/BuildHelper";
 import { ExpansionLogos } from "../../assets/gw2Assets/expansionLogos/ExpansionLogos";
 import paintSmear from "../../assets/gw2Assets/accents/pattern4.png";
 import BuildsUtil from "../../assets/BuildsUtil";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 interface Props {
     title:string;
@@ -15,34 +15,30 @@ interface Props {
 
 const styles = {
     card: {objectFit:"cover", opacity: "0.5", height: "350px"} as React.CSSProperties,
-    hoverCard: {objectFit:"cover", opacity: "1", height: "350px"} as React.CSSProperties
+    hoverCard: {objectFit:"cover", opacity: "1", height: "350px", cursor:"pointer"} as React.CSSProperties
 }
 
 function BuildCard(props: Props) {
     const [hoverState, setHoverState] = useState(false);
 
-    return <Card data-bs-theme="dark" onMouseEnter={() => setHoverState(true)} onMouseLeave={() => setHoverState(false)} onClick={props.onClick} className="specCard">
-        <Card.Img className="specCardImage"  variant="top" style={hoverState ? styles.hoverCard : styles.card} src={props.image} />
-        <Card.ImgOverlay>
-        <Card.Body className="w-100 h-100 p-0 m-0">
-            <Image src={paintSmear} style={{ maxWidth:"300px", width:"100%", position:"absolute", zIndex:0, margin:-16, opacity:0.7}}></Image>
+    useEffect(() => {
+        document.body.style.cursor = hoverState ? 'pointer' : 'auto'
+      }, [hoverState])
 
-            {/*CONTAINER FOR THE CARD: ROUNDED EDGES, HOVER EFFECTS, ETC*/}
-            <Container style={{ padding:"0px", zIndex:1, position:"absolute" }}>
+    return <Card data-bs-theme="dark" onMouseEnter={() => setHoverState(true)} onMouseLeave={() => setHoverState(false)} onClick={props.onClick} className="specCard">
+        <Card.Img className="specCardImage"  variant="top" style={{objectFit:"cover", opacity: hoverState ? "1" : "0.5", height: "350px"}} src={props.image} />
+        <Card.ImgOverlay className="p-0 m-0">
+        <Card.Body className="w-100 h-100 p-0 m-0">
+            <Image src={paintSmear} style={{ borderRadius:"var(--bs-border-radius)", maxWidth:"300px", width:"100%", position:"absolute", zIndex:0, opacity:0.7}}></Image>
+
+            <Container style={{ padding:"16px", zIndex:1, position:"absolute" }}>
                 <Row style={{ margin:"0px", height: '268px' }}>
-                {/*BUILD INFO ROW*/}
                     <Col style={{color:"black"}}>
-                    {/*BUILD TITLE AND SUBTITLE*/}
                     <h5>{props.title}</h5>
                     <h6>{props.secondary}</h6>
                     </Col>
-                    <Col>
-                    {/*BUILD DMG TYPE ICON AND OPTIONAL BOON ICON*/}
-                    
-                    </Col>
                 </Row>
                 <Row className="justify-content-start" style={{  margin:"0px", height: '50px'}}>
-                {/*EXPANSION INFO ROW*/}
                     {Array.from({ length: BuildsUtil.getRequiredExpansions(props.build).length}).map((_, idx) => (
                     <Col className="justify-self-end" style={{ height: "50px", maxWidth: '60px', padding:"0px" }} key={idx}>
                         <Image 
