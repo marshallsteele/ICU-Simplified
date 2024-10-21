95705bddf25c3ab9388e83f08eb760e1b2297e71
Marshall Steele <marshallsteele123@hotmail.com>
2024-10-21 14:37:32 -0400

    Fixed mapping issue due to hashing assets in vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/ICU-Simplified/"
+  base: "/ICU-Simplified/",
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: `assets/[name].[ext]`
+      }
+    }
+  }
 })
