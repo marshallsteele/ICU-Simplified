bbec090f7788d7264fc1581f03fd912b47b4ad7c
Marshall Steele <marshallsteele123@hotmail.com>
2025-11-10 17:52:44 -0500

    Added VoE images

--- src/assets/gw2Assets/specs/SpecImages.tsx
+++ src/assets/gw2Assets/specs/SpecImages.tsx
@@ -35,115 +35,151 @@ import Necromancer_icon from "./Necromancer/Necromancer_icon.jpg";
 import Reaper_icon from "./Necromancer/Reaper_icon.jpg";
 import Scourge_icon from "./Necromancer/Scourge_icon.jpg";
 import Harbinger_icon from "./Necromancer/Harbinger_icon.jpg";
+import Evoker_icon from "./Elementalist/Evoker_icon.jpg";
+import Amalgam_icon from "./Engineer/Amalgam_icon.jpg";
+import Luminary_icon from "./Guardian/Luminary_icon.jpg";
+import Troubadour_icon from "./Mesmer/Troubadour_icon.jpg";
+import Ritualist_icon from "./Necromancer/Ritualist_icon.jpg";
+import Galeshot_icon from "./Ranger/Galeshot_icon.jpg";
+import Conduit_icon from "./Revenant/Conduit_icon.jpg";
+import Antiquary_icon from "./Thief/Antiquary_icon.jpg";
+import Paragon_icon from "./Warrior/Paragon_icon.jpg";
 //#endregion
 
 export var specImages = {
-    "Guardian": {
-        icon:Guardian_icon
-    }, 
-    "Dragonhunter": {
-        icon:Dragonhunter_icon
-    }, 
-    "Firebrand": {
-        icon:Firebrand_icon
-    }, 
-    "Willbender": {
-        icon:Willbender_icon
-    }, 
-    "Revenant": {
-        icon:Revenant_icon
-    }, 
-    "Herald": {
-        icon:Herald_icon
-    }, 
-    "Renegade": {
-        icon:Renegade_icon
-    }, 
-    "Vindicator": {
-        icon:Vindicator_icon
-    }, 
-    "Warrior": {
-        icon:Warrior_icon
-    }, 
-    "Berserker": {
-        icon:Berserker_icon
-    }, 
-    "Spellbreaker": {
-        icon:Spellbreaker_icon
-    }, 
-    "Bladesworn": {
-        icon:Bladesworn_icon
-    }, 
-    "Engineer": {
-        icon:Engineer_icon
-    }, 
-    "Scrapper": {
-        icon:Scrapper_icon
-    }, 
-    "Holosmith": {
-        icon:Holosmith_icon
-    }, 
-    "Mechanist": {
-        icon:Mechanist_icon
-    }, 
-    "Ranger": {
-        icon:Ranger_icon
-    }, 
-    "Druid": {
-        icon:Druid_icon
-    }, 
-    "Soulbeast": {
-        icon:Soulbeast_icon
-    }, 
-    "Untamed": {
-        icon:Untamed_icon
-    }, 
-    "Thief": {
-        icon:Thief_icon
-    }, 
-    "Daredevil": {
-        icon:Daredevil_icon
-    }, 
-    "Deadeye": {
-        icon:Deadeye_icon
-    }, 
-    "Specter": {
-        icon:Specter_icon
-    }, 
-    "Elementalist": {
-        icon:Elementalist_icon
-    }, 
-    "Tempest": {
-        icon:Tempest_icon
-    }, 
-    "Weaver": {
-        icon:Weaver_icon
-    }, 
-    "Catalyst": {
-        icon:Catalyst_icon
-    }, 
-    "Mesmer": {
-        icon:Mesmer_icon
-    }, 
-    "Chronomancer": {
-        icon:Chronomancer_icon
-    }, 
-    "Mirage": {
-        icon:Mirage_icon
-    }, 
-    "Virtuoso": {
-        icon:Virtuoso_icon
-    }, 
-    "Necromancer": {
-        icon:Necromancer_icon
-    }, 
-    "Reaper": {
-        icon:Reaper_icon
-    }, 
-    "Scourge": {
-        icon:Scourge_icon
-    }, 
-    "Harbinger": {
-        icon:Harbinger_icon
-    }
-}
+  Guardian: {
+    icon: Guardian_icon,
+  },
+  Dragonhunter: {
+    icon: Dragonhunter_icon,
+  },
+  Firebrand: {
+    icon: Firebrand_icon,
+  },
+  Willbender: {
+    icon: Willbender_icon,
+  },
+  Revenant: {
+    icon: Revenant_icon,
+  },
+  Herald: {
+    icon: Herald_icon,
+  },
+  Renegade: {
+    icon: Renegade_icon,
+  },
+  Vindicator: {
+    icon: Vindicator_icon,
+  },
+  Warrior: {
+    icon: Warrior_icon,
+  },
+  Berserker: {
+    icon: Berserker_icon,
+  },
+  Spellbreaker: {
+    icon: Spellbreaker_icon,
+  },
+  Bladesworn: {
+    icon: Bladesworn_icon,
+  },
+  Engineer: {
+    icon: Engineer_icon,
+  },
+  Scrapper: {
+    icon: Scrapper_icon,
+  },
+  Holosmith: {
+    icon: Holosmith_icon,
+  },
+  Mechanist: {
+    icon: Mechanist_icon,
+  },
+  Ranger: {
+    icon: Ranger_icon,
+  },
+  Druid: {
+    icon: Druid_icon,
+  },
+  Soulbeast: {
+    icon: Soulbeast_icon,
+  },
+  Untamed: {
+    icon: Untamed_icon,
+  },
+  Thief: {
+    icon: Thief_icon,
+  },
+  Daredevil: {
+    icon: Daredevil_icon,
+  },
+  Deadeye: {
+    icon: Deadeye_icon,
+  },
+  Specter: {
+    icon: Specter_icon,
+  },
+  Elementalist: {
+    icon: Elementalist_icon,
+  },
+  Tempest: {
+    icon: Tempest_icon,
+  },
+  Weaver: {
+    icon: Weaver_icon,
+  },
+  Catalyst: {
+    icon: Catalyst_icon,
+  },
+  Mesmer: {
+    icon: Mesmer_icon,
+  },
+  Chronomancer: {
+    icon: Chronomancer_icon,
+  },
+  Mirage: {
+    icon: Mirage_icon,
+  },
+  Virtuoso: {
+    icon: Virtuoso_icon,
+  },
+  Necromancer: {
+    icon: Necromancer_icon,
+  },
+  Reaper: {
+    icon: Reaper_icon,
+  },
+  Scourge: {
+    icon: Scourge_icon,
+  },
+  Harbinger: {
+    icon: Harbinger_icon,
+  },
+  Evoker: {
+    icon: Evoker_icon,
+  },
+  Amalgam: {
+    icon: Amalgam_icon,
+  },
+  Luminary: {
+    icon: Luminary_icon,
+  },
+  Troubadour: {
+    icon: Troubadour_icon,
+  },
+  Ritualist: {
+    icon: Ritualist_icon,
+  },
+  Galeshot: {
+    icon: Galeshot_icon,
+  },
+  Conduit: {
+    icon: Conduit_icon,
+  },
+  Antiquary: {
+    icon: Antiquary_icon,
+  },
+  Paragon: {
+    icon: Paragon_icon,
+  },
+};
